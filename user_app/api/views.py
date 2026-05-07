eff2bc344a29f6e5dc9a33c32294e3b5ae835361
MohammadHossein Maleki <147469128+maleky83@users.noreply.github.com>
2026-05-07 22:13:31 +0330

    update authentication user register

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from .serializers import RegistrationSerializer
 from rest_framework.response import Response
+from rest_framework.authtoken.models import Token
 
 
 @api_view(['POST',])
@@ -9,8 +10,19 @@ def registration_view(request):
     if request.method == 'POST':
         serializer = RegistrationSerializer(data=request.data)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+        data = {}
 
-        return Response(serializer.errors)
+        if serializer.is_valid():
+            account = serializer.save()
+
+            data['username'] = account.username
+            data['email'] = account.email
+            data['response'] = 'registration successful'
+
+            token = Token.objects.get(user=account).key
+            data['token'] = token
+
+        else:
+            data = serializer.errors
+
+        return Response(data)
